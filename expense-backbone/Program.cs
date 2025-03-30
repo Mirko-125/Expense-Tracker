d55a57e11f5022c2f7536517f0a533dace239726
Mirko <mirkoat21@gmail.com>
2025-03-30 14:14:38 +0200

    add function is done, update is wip

--- expense-backbone/Program.cs
+++ expense-backbone/Program.cs
@@ -43,13 +43,45 @@ namespace ExpenseTracker
             File.WriteAllText(path, json);
         }
 
-        static void AddExpense(List<Expense> expenses)
+        static void AddExpense(List<Expense> expenses, string description, string amount)
         {
+            try
+            {
+                int newId = expenses.Count > 0 ? expenses[^1].ID + 1 : 1;
+                int cash = Int32.Parse(amount);
+                var newExpense = new Expense
+                {
+                    ID = newId,
+                    Description = description,
+                    Amount = cash,
+                    Date = DateTime.UtcNow
+                };
 
+                expenses.Add(newExpense);
+                Console.WriteLine("Expense added successfully (ID: 1), use list for details");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a number for the cash amount");
+            }
         }
-        static void UpdateExpense(List<Expense> expenses)
+        static void UpdateExpense(List<Expense> expenses, string stringId)
         {
-
+            try
+            {
+                int id = Int32.Parse(stringId);
+                var expense = expenses.Find(e => e.ID == id);
+                if (expense == null)
+                {
+                    Console.WriteLine("Expense with ID " + id + " not found.");
+                    return;
+                }
+                // you are here
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a valid expense ID");
+            }
         }
         static void DeleteExpense(List<Expense> expenses)
         {
